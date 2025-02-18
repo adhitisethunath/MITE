2b9edaccda122921f51e9b52ce35acc177e1c3ed
adhitisethunath <190721208+adhitisethunath@users.noreply.github.com>
2025-02-18 22:27:46 +0530

    Create factorial.c

---
+++ day 2/factorial.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+long int factorial(int number){
+  if(number >= 1)
+   return number*factorial(number-1);
+  else
+    return 1;
+}
+
+int main(){
+  int number;
+
+   printf("Enter a number: ");
+   scanf("%d", &number);
+
+   printf("Factorial of %d is: %ld", number, factorial(number));
+   return 0;
+}
